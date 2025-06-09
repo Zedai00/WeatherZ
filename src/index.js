3bd1e840505ab3c3577a9c1cc1837f142e4e9b02
zedai00 <33319711+Zedai00@users.noreply.github.com>
2025-06-09 18:01:06 +0530

    Process Weather Info

--- src/index.js
+++ src/index.js
@@ -5,3 +5,28 @@ async function getWeatherInfo(location) {
   const response = await fetch(url, { mode: 'cors' });
   return response.json();
 }
+
+function processWeatherData(weatherData, unit) {
+  const curr = weatherData.currentConditions;
+
+  let temp = curr.temp;
+  let feels = curr.feelslike;
+
+  if (unit === 'C') {
+    temp = ((temp - 32) * 5 / 9).toFixed(1);
+    feels = ((feels - 32) * 5 / 9).toFixed(1);
+  } else {
+    temp = temp.toFixed(1);
+    feels = feels.toFixed(1);
+  }
+
+  return {
+    address: weatherData.resolvedAddress,
+    desc: weatherData.description,
+    condition: curr.conditions,
+    feels,
+    humid: curr.humidity,
+    icon: curr.icon,
+    temp
+  };
+}
